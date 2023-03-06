15ea5f17e0dde772d0b3aee8384c2736398a5ec3
Aravindan R <85152278+RAravindDS@users.noreply.github.com>
2023-03-06 03:36:34 +0000

    Initial commit

--- Main.java
+++ Main.java
@@ -1,7 +1,20 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-    System.out.println("This is my first program in REPLS"); 
-    System.out.println("Just chekcing"); 
+
+    // Creating a String outside the pool! 
+    String  b = new String("aravind"); 
+    String a = new String("aravind"); 
+    // a == b -> false 
+
+    // Creating a String with String pool 
+    String c = "aravind"; 
+    String d = "aravind"; 
+    // c == d -> true 
+
+    // Checking only values not objects
+    System.out.println("Checking only values: "+c.equals(d));
+    System.out.println("Outside the Stringpool" + a == b); 
+    System.out.println("Inside the Stringpool" + c == d); 
+    
   }
 }
